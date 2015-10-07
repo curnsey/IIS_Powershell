bf28ddf9f28a6edbae16564422ffdf73514fbd04
Adam Currins <curnsey.dev@gmail.com>
2015-10-07 16:58:59 -0400

    Update List-WebApplications.ps1

--- List-WebApplications.ps1
+++ List-WebApplications.ps1
@@ -3,34 +3,29 @@
    List web applications on server
 .DESCRIPTION
    This script lists all web applications in IIS on the given server
-.PARAMETER
-  -Server: Servername
-.PARAMETER
-  -UserName: name of user you want to connect with. Default is Administrator
-.PARAMETER
-  -List: List of properties to pull back. Default list is Path, PhysicalPath, ApplicationPool, EnabledProtocols
 .EXAMPLE
-  PS C:\> .\List-WebApplications.ps1 -Server webserver1 -UserName "Admin"
+  .\List-WebApplications.ps1 -Server webserver1 -UserName "Admin" -List "Path", "PhysicalPath"
 #>
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$Server,
-    [string]$UserName = "Administrator",
-    [string[]]$List = ("Path", "PhysicalPath", "ApplicationPool", "EnabledProtocols")
-)
+ [CmdletBinding()]
+ param(
+     [Parameter(Mandatory=$True)]
+     [string]$Server,
+     [string]$UserName = "Administrator",
+     [string[]]$List = ("Path", "PhysicalPath", "ApplicationPool", "EnabledProtocols")
+ )
 
-#Create credentials for session
-$cred = (Get-Credential "$Server\$UserName")
+ #Create credentials for session
+ $cred = (Get-Credential "$Server\$UserName")
+ 
+ #Create Session
+ $session = new-pssession -computername $Server -credential $cred
 
-#Create Session
-$session = new-pssession -computername $Server -credential $cred
+ #Import the Web Administration tools for Source server
+ #Any commands now ran for WebAdmin will be ran on Staging Server
+ Import-PSSession -Session $session -Module WebAdministrAtion -prefix iis
 
-#Import the Web Administration tools for Source server
-#Any commands now ran for WebAdministration Tools will be ran on Server (iis as prefix)
-Import-PSSession -Session $session -Module WebAdministrAtion -prefix iis
+ #Provide user with list of Applications
+ Get-iisWebApplication | select $List | Format-Table
 
-#Provide Developer with list of Applications
-Get-iisWebApplication | select "Path","PhysicalPath","ApplicationPool","enabledprotocols" | Format-Table
-
-#Lastly, remove the session
-Remove-PSSession -session $session
+ #Lastly, remove the session
+ Remove-PSSession -session $session
